11f5ce54e649f9242c8ce5835c99ed93b2a91472
Vi <gimmefear@gmail.com>
2023-03-27 22:32:23 +0600

    add get events method

--- storer/event.go
+++ storer/event.go
@@ -8,6 +8,7 @@ import (
 )
 
 type EventStorage interface {
+	GetEvents() ([]*types.Event, error)
 	GetEvent(id int) (*types.Event, error)
 	AddEvent(event *types.Event) error
 	DeleteEvent(id int) error
@@ -22,6 +23,22 @@ func NewEventPostgresStorage(db *sql.DB) *EventPostgresStorage {
 	return &EventPostgresStorage{db: db}
 }
 
+func (s *EventPostgresStorage) GetEvents() ([]*types.Event, error) {
+	rows, err := s.db.Query("SELECT * FROM events")
+	if err != nil {
+		return nil, err
+	}
+	events := []*types.Event{}
+	for rows.Next() {
+		event, err := scanIntoEvent(rows)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, nil
+}
+
 func (s *EventPostgresStorage) GetEvent(id int) (*types.Event, error) {
 	rows, err := s.db.Query("SELECT * FROM events WHERE id = $1", id)
 	if err != nil {
@@ -34,7 +51,12 @@ func (s *EventPostgresStorage) GetEvent(id int) (*types.Event, error) {
 }
 
 func (s *EventPostgresStorage) AddEvent(event *types.Event) error {
-	_, err := s.db.Exec("INSERT INTO events (name, description, owner_id) VALUES ($1, $2, $3)", event.Name, event.Description, event.OwnerID)
+	_, err := s.db.Exec(
+		"INSERT INTO events (name, description, owner_id) VALUES ($1, $2, $3)",
+		event.Name,
+		event.Description,
+		event.OwnerID,
+	)
 	return err
 }
 
@@ -44,14 +66,20 @@ func (s *EventPostgresStorage) DeleteEvent(id int) error {
 }
 
 func (s *EventPostgresStorage) UpdateEvent(event *types.Event) error {
-	_, err := s.db.Exec("UPDATE events SET name = $1, description = $2 WHERE id = $3", event.Name, event.Description, event.ID)
+	_, err := s.db.Exec(
+		"UPDATE events SET name = $1, description = $2, participants = &3 WHERE id = $4",
+		event.Name,
+		event.Description,
+		event.Participants,
+		event.ID,
+	)
 	return err
 }
 
 func scanIntoEvent(rows *sql.Rows) (*types.Event, error) {
 	e := &types.Event{}
 
-	if err := rows.Scan(&e.ID, &e.Name, &e.Description, &e.OwnerID); err != nil {
+	if err := rows.Scan(&e.ID, &e.Name, &e.Description, &e.OwnerID, &e.Participants); err != nil {
 		return nil, err
 	}
 	return e, nil
